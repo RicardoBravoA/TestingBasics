4b2290ce804cf775aa480b27363fa27c7ac594c2
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-02 16:55:48 -0500

    refactor: Rename fragment

--- app/src/main/java/com/udacity/testing/basics/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/udacity/testing/basics/statistics/StatisticsFragment.kt
@@ -1,4 +1,4 @@
-package com.udacity.testing.basics
+package com.udacity.testing.basics.statistics
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,18 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.udacity.testing.basics.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class StatisticsFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_statistics, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
