80fa40e87578908623f95a0002fadfc435fd3f97
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-03 16:41:24 -0500

    feat: Add statistics fragment

--- app/src/main/java/com/udacity/testing/basics/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/udacity/testing/basics/statistics/StatisticsFragment.kt
@@ -5,18 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.udacity.testing.basics.R
+import com.udacity.testing.basics.databinding.FragmentStatisticsBinding
+import com.udacity.testing.basics.util.setupRefreshLayout
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * Main UI for the statistics screen.
  */
 class StatisticsFragment : Fragment() {
+
+    private lateinit var viewDataBinding: FragmentStatisticsBinding
+
+    private val statisticsViewModel by viewModels<StatisticsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false)
+        viewDataBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_statistics, container,
+            false
+        )
+        return viewDataBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewDataBinding.viewModel = statisticsViewModel
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        this.setupRefreshLayout(viewDataBinding.refreshLayout)
     }
 }
